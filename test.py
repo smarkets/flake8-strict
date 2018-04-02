8db9b10978909a4a3a3adbb754139fd6278ad7c8
Jaime Lennox <jaime.lennox@smarkets.com>
2018-04-03 00:24:12 +0100

    Add back support for trailing commas after *args/**kwargs
    This adds back the changes from
    d91fc13904ad4427ac3eec2ac99d2c66e0942949, along with some extra python
    3.6 specific tests to ensure they work correctly this time.
    
    This can now be done with the use of blib2to3 instead of lib2to3.
    
    We also let travis test on 3.6 to ensure everything is ok.

--- test.py
+++ test.py
@@ -1,22 +1,34 @@
+import platform
 import re
+import sys
+
+from distutils.version import LooseVersion
 
 from nose.tools import eq_
 
 from flake8_strict import _process_code
 
 
-def test_processing():
-    with open('test_data.py', 'rt') as f:
+def _test_processing(test_data_path):
+    with open(test_data_path, 'rt') as f:
         code = f.read()
 
     code = code.strip()
 
     expected_errors = set()
     for lineno, line in enumerate(code.splitlines()):
+        include_errors = True
         match = re.search(r'  # (.*)$', line.strip('\n'))
         if match:
-            for error_code in match.group(1).split():
-                expected_errors.add((lineno + 1, error_code))
+            for code_or_version in match.group(1).split():
+                version_match = re.search(r'py(.*):', code_or_version)
+                if version_match:
+                    include_errors = (
+                        LooseVersion(platform.python_version()) >=
+                        LooseVersion(version_match.group(1))
+                    )
+                elif include_errors:
+                    expected_errors.add((lineno + 1, code_or_version))
 
     actual_errors = {
         (line, error_code.name)
@@ -27,3 +39,14 @@ def test_processing():
     false_negatives = actual_errors - expected_errors
 
     eq_((not_caught, false_negatives), (set(), set()))
+
+
+def test_processing_py2_py3():
+    """Test the library against Python 2/3 compatible syntaxes."""
+    _test_processing('test_data_py2_py3.py')
+
+
+if sys.version_info >= (3, 6):
+    def test_processing_py36():
+        """Test the library against Python 3.6+ specific syntaxes."""
+        _test_processing('test_data_py36.py')
